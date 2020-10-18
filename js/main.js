06a17ebc8f5371312d30015202d9601735d47313
ppopow <pawel.popow@gmail.com>
2020-10-18 22:58:10 +0200

    nowe zadanie39

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 6;
+let num4 = 7;
+let num5 = 9;
+
+let array = [num1, num2, num3, num4, num5];
+
+function array (num1, num2, num3, num4, num5) {
+    let result = num1 * num2 * num3 * num4 * num5;
+
+    return result;
+}
+console.log ( array(2, 4, 6, 7, 9) );
